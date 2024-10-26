e054bfa823bad62921c3b36b1cd8134a05deb695
Anastasiya Gurianova <gurianova.nast@gmail.com>
2024-10-26 18:04:43 +0300

    build: add Prettier and integrate with ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,39 @@
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
 export default [
 	{
-	  files: ['**/*.{js,jsx,ts,tsx}'],
-	  languageOptions: {
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		ignores: ['dist', 'node_modules'],
+
+		plugins: {
+			prettier: prettierPlugin,
+		},
+
+		languageOptions: {
 			parserOptions: {
-		  ecmaVersion: 2021, 
-		  sourceType: 'module',
-			},
-	  },
-	  rules: {
+		    ecmaVersion: 2021, 
+		    sourceType: 'module',
+		    },
+	    },
+
+		rules: {
+			...eslintConfigPrettier.rules,
 			'indent': ['error', 'tab'],
 			'no-unused-vars': 'warn',
 			'no-console': 'off',
 			'no-debugger': 'warn',
 			'semi': ['error', 'always'],
 			'quotes': ['error', 'single'],
-	  },
-	  settings: {
+			"prettier/prettier": "error",
+		},
+
+		settings: {
 			'import/resolver': {
-		  webpack: {
+		        webpack: {
 					config: 'webpack.config.dev.js',
-		  },
+		        },
 			},
-	  },
+	    },
 	},
 ];
